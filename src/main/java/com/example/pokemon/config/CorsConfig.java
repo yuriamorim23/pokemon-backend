e1eff4a3d421c706e9e473908610fa01ea6074e3
Yuri Amorim <yuricalixto.uk@gmail.com>
2025-01-18 02:42:29 +0000

    Cors

--- src/main/java/com/example/pokemon/config/CorsConfig.java
+++ src/main/java/com/example/pokemon/config/CorsConfig.java
@@ -1,5 +1,7 @@
 package com.example.pokemon.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -13,10 +15,9 @@ public class CorsConfig {
     CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:4200");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
+        config.setAllowedOrigins(List.of("*")); 
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowedMethods(List.of("*"));
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
